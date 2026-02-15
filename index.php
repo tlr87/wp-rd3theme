c985e8904b38289e55f1898c0891a84435c8adb9
Tom Revill <tom.revill@gmail.com>
2026-02-15 15:28:05 +1300

    Left header and Footer change. Fixes with index.php and page.php

--- index.php
+++ index.php
@@ -1,24 +1,28 @@
-<?php get_header(); ?>
+<div class="Main-container">
 
-<?php if ( get_theme_mod('rd3_show_breadcrumbs', true) ) : ?>
-    <?php rd3_breadcrumbs(); ?>
-<?php endif; ?>
+    <?php get_header(); ?>
 
-<div class="content-area" style="flex-direction: <?php echo get_theme_mod('rd3_sidebar_position','left') === 'left'?'row':'row-reverse'; ?>;">
-    <?php get_sidebar(); ?>
+    <?php if ( get_theme_mod('rd3_show_breadcrumbs', true) ) : ?>
+        
+    <?php endif; ?>
 
-    <main class="site-main">
-        <?php if ( have_posts() ) : ?>
-            <?php while ( have_posts() ) : the_post(); ?>
-                <article class="post">
-                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <?php the_excerpt(); ?>
-                </article>
-            <?php endwhile; ?>
-        <?php else: ?>
-            <p><?php _e('No posts found.', 'rd3starter'); ?></p>
-        <?php endif; ?>
-    </main>
+    <div class="content-area" style="flex-direction: <?php echo get_theme_mod('rd3_sidebar_position','left') === 'left'?'row':'row-reverse'; ?>;">
+        <?php get_sidebar(); ?>
+    
+        <main class="site-main">
+                <?php rd3_breadcrumbs(); ?>
+            <?php if ( have_posts() ) : ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                    <article class="post">
+                        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <?php the_excerpt(); ?>
+                    </article>
+                <?php endwhile; ?>
+            <?php else: ?>
+                <p><?php _e('No posts found.', 'rd3starter'); ?></p>
+            <?php endif; ?>
+        </main>
+    </div>
+
+    <?php get_footer(); ?>
 </div>
-
-<?php get_footer(); ?>
--- page.php
+++ page.php
@@ -1,14 +1,16 @@
+
+<div class="Main-container">
 <?php get_header(); ?>
 
 <p><?php if ( get_theme_mod('rd3_show_breadcrumbs', true) ) : ?>
-    <?php rd3_breadcrumbs(); ?>
+
 <?php endif; ?></p>
 
 
 <?php while ( have_posts() ) : the_post(); ?>
 
 <article class="page">
-
+    <?php rd3_breadcrumbs(); ?>
     <h1><?php the_title(); ?></h1>
 
     <?php the_content(); ?>
@@ -18,3 +20,4 @@
 <?php endwhile; ?>
 
 <?php get_footer(); ?>
+</div>
