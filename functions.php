abe0e71d073bf727717c088f7f18076a55674eba
Tom Revill <tom.revill@gmail.com>
2026-02-16 16:17:16 +1300

    Cant add Custom CSS to Layout Settings → Upload Custom Layout CSS. Fixes CSS upload.

--- functions.php
+++ functions.php
@@ -357,3 +357,24 @@ function rd3_breadcrumbs()
 
     echo '</p>';
 }
+
+// Allow CSS uploads
+function rd3_allow_css_uploads( $mimes ) {
+    $mimes['css'] = 'text/css';
+    return $mimes;
+}
+add_filter( 'upload_mimes', 'rd3_allow_css_uploads' );
+
+// Fix WordPress filetype checking (WP 5.0+)
+function rd3_fix_css_mime_check( $data, $file, $filename, $mimes ) {
+
+    $ext = pathinfo( $filename, PATHINFO_EXTENSION );
+
+    if ( $ext === 'css' ) {
+        $data['ext']  = 'css';
+        $data['type'] = 'text/css';
+    }
+
+    return $data;
+}
+add_filter( 'wp_check_filetype_and_ext', 'rd3_fix_css_mime_check', 10, 4 );
