f98309f6b66c074ad28525f372886d0b86dc1b91
Tom Revill <tom.revill@gmail.com>
2026-02-17 14:34:24 +1300

    maintenance.php fixes

--- maintenance.php
+++ maintenance.php
@@ -1,28 +1,65 @@
 <?php
-// Get the back message from Customizer
-$back_message = get_theme_mod('rd3_maintenance_back_message', 'We are back!');
+/**
+ * Maintenance Page Template
+ */
+
+// Get Customizer settings
+$bg_color        = get_theme_mod('rd3_maintenance_bg', '#f7f7f7');
+$text_color      = get_theme_mod('rd3_maintenance_text', '#333');
+$logo            = get_theme_mod('rd3_maintenance_logo', '');
+$title           = get_theme_mod('rd3_maintenance_title', 'Site Under Maintenance');
+$message         = get_theme_mod('rd3_maintenance_message', 'We’re making improvements. Please check back soon.');
+$countdown_date  = get_theme_mod('rd3_maintenance_countdown', date('Y-m-d H:i:s', strtotime('+3 days')));
+$back_message    = get_theme_mod('rd3_maintenance_back_message', 'We are back!');
 ?>
-<script>
-// Countdown timer using Customizer date
-const countdownDate = new Date("<?php echo esc_js($countdown_date); ?>").getTime();
-const countdownEl = document.getElementById('countdown');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo esc_html($title); ?></title>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/maintenance.css">
+    <style>
+        body {
+            background-color: <?php echo $bg_color; ?>;
+            color: <?php echo $text_color; ?>;
+        }
+    </style>
+</head>
+<body>
+    <div class="maintenance-container">
+        <?php if ($logo): ?>
+            <img src="<?php echo esc_url($logo); ?>" alt="Logo" class="maintenance-logo">
+        <?php endif; ?>
 
-function updateCountdown() {
-    const now = new Date().getTime();
-    const distance = countdownDate - now;
+        <h1><?php echo esc_html($title); ?></h1>
+        <p><?php echo esc_html($message); ?></p>
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000*60*60));
-    const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-    const seconds = Math.floor((distance % (1000*60)) / 1000);
+        <div id="countdown"></div>
+    </div>
 
-    if (distance > 0) {
-        countdownEl.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    } else {
-        countdownEl.innerHTML = "<?php echo esc_js($back_message); ?>";
-    }
-}
+    <script>
+        // Countdown timer
+        const countdownDate = new Date("<?php echo esc_js($countdown_date); ?>").getTime();
+        const countdownEl = document.getElementById('countdown');
 
-setInterval(updateCountdown, 1000);
-updateCountdown();
-</script>
+        function updateCountdown() {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+
+            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000*60*60));
+            const minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
+            const seconds = Math.floor((distance % (1000*60)) / 1000);
+
+            if (distance > 0) {
+                countdownEl.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+            } else {
+                countdownEl.innerHTML = "<?php echo esc_js($back_message); ?>";
+            }
+        }
+
+        setInterval(updateCountdown, 1000);
+        updateCountdown();
+    </script>
+</body>
+</html>
